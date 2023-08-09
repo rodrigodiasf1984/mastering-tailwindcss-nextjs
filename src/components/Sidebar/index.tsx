e33be786ab132df102d7b338fa8e0e5fffdf201c
rodrigodiasf1984 <rodrigodiasf@gmail.com>
2023-08-09 20:43:37 +0200

    chore: add logo

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Logo } from './Logo'
+
+const Sidebar = () => {
+  return (
+    <aside className="border-r border-zinc-200 px-5 py-8 ">
+      <Logo />
+    </aside>
+  )
+}
+
+export default Sidebar
