1115c040574523895e9921d30bfb065bef722cf5
rodrigodiasf1984 <rodrigodiasf@gmail.com>
2023-08-10 08:53:18 +0200

    refactor: downgrade the version of lucide-react due a bug with next.js

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Logo } from './Logo'
+import { Search } from 'lucide-react'
 
 const Sidebar = () => {
   return (
     <aside className="border-r border-zinc-200 px-5 py-8 ">
       <Logo />
+      <div className="flex w-full items-center gap-2 rounded-lg border border-e-zinc-300 px-3 py-2 shadow-sm">
+        <Search />
+      </div>
     </aside>
   )
 }
