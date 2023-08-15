fc50c65f16253dc49d9ba15221b0e71ecbd1b8e4
rodrigodiasf1984 <rodrigodiasf@gmail.com>
2023-08-15 16:42:47 +0200

    chore: add header if the form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,34 @@ export default function Home() {
     <>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
       <SettingsTabs />
+      <div className="mt-6 flex flex-col">
+        <div className="flex flex-row items-center justify-between border-b border-zinc-200 pb-5">
+          <div className="space-y-1">
+            <h2 className="textlg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update ypur photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50 "
+              type="button"
+            >
+              Cancel
+            </button>
+            <button
+              className="hover: rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+              type="submit"
+              form="settings"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+        <form className="mt-6 flex w-full flex-col " id="settings" action="">
+          form
+        </form>
+      </div>
     </>
   )
 }
