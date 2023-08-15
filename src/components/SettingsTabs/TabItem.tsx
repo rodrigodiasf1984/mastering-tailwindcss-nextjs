289c5f63bef04ac4dd431f0b5b727d6036b21284
rodrigodiasf1984 <rodrigodiasf@gmail.com>
2023-08-15 16:30:50 +0200

    chore; animate the tabs

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 
 type TabItemProps = {
   value: string
@@ -15,9 +16,15 @@ export default function TabItem({
   return (
     <Tabs.Trigger
       value={value}
-      className="px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
+      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-800"
     >
       <span>{title}</span>
+      {isSelected && (
+        <motion.div
+          layoutId="activeTab"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+        />
+      )}
     </Tabs.Trigger>
   )
 }
--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,24 +1,70 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import * as Tabs from '@radix-ui/react-tabs'
 import TabItem from './TabItem'
 
 export default function SettingsTabs() {
+  const [currentTab, setCurrentTab] = useState('tab1')
+
   return (
     <>
-      <Tabs.Root defaultValue="tab1">
+      <Tabs.Root
+        defaultValue="tab1"
+        value={currentTab}
+        onValueChange={setCurrentTab}
+      >
         <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-b-zinc-200">
-          <TabItem title="My details" value="tab1" isSelected />
-          <TabItem title="Profile" value="tab2" />
-          <TabItem title="Password" value="tab3" />
-          <TabItem title="Team" value="tab4" />
-          <TabItem title="Plan" value="tab5" />
-          <TabItem title="Billing" value="tab6" />
-          <TabItem title="Email" value="tab7" />
-          <TabItem title="Notifications" value="tab8" />
-          <TabItem title="Integrations" value="tab9" />
-          <TabItem title="Api" value="tab10" />
+          <TabItem
+            title="My details"
+            value="tab1"
+            isSelected={currentTab === 'tab1'}
+          />
+          <TabItem
+            title="Profile"
+            value="tab2"
+            isSelected={currentTab === 'tab2'}
+          />
+          <TabItem
+            title="Password"
+            value="tab3"
+            isSelected={currentTab === 'tab3'}
+          />
+          <TabItem
+            title="Team"
+            value="tab4"
+            isSelected={currentTab === 'tab4'}
+          />
+          <TabItem
+            title="Plan"
+            value="tab5"
+            isSelected={currentTab === 'tab5'}
+          />
+          <TabItem
+            title="Billing"
+            value="tab6"
+            isSelected={currentTab === 'tab6'}
+          />
+          <TabItem
+            title="Email"
+            value="tab7"
+            isSelected={currentTab === 'tab7'}
+          />
+          <TabItem
+            title="Notifications"
+            value="tab8"
+            isSelected={currentTab === 'tab8'}
+          />
+          <TabItem
+            title="Integrations"
+            value="tab9"
+            isSelected={currentTab === 'tab9'}
+          />
+          <TabItem
+            title="Api"
+            value="tab10"
+            isSelected={currentTab === 'tab10'}
+          />
         </Tabs.List>
       </Tabs.Root>
     </>
